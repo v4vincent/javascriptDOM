f71347f263108dc8b0b6f0c92e1afa0b96efe5c1
v4vincent <vermillionvincent@gmail.com>
2024-03-11 22:31:59 -0500

    first commit

---
+++ modifying-dom-node-attributes.js
@@ -0,0 +1,9 @@
+
+let changePic = document.querySelector("img");
+changePic.src = "nikki.jpg";
+changePic.alt = "pic of nik";
+changePic.removeAttribute("height")
+changePic.width="1080";
+
+let p = document.querySelector("p");
+p.style = "color: green";
---
+++ script3.js
@@ -0,0 +1,7 @@
+
+let para = document.getElementById("early_languages");
+let listItems = document.querySelectorAll("li");
+
+console.log(para);
+console.log(listItems);
+console.log(document.getElementsByTagName("li")[0]);
